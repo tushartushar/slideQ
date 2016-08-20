aa7881cad7948c82845a16a228c0e5368d30b0d1
Tushar Sharma <000.tushar@gmail.com>
2016-08-20 10:43:22 +0300

    first clean version

---
+++ slideQ/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slideQ
+{
+    public class Constants
+    {
+        public static string AppName = "slideQ";
+    }
+}
