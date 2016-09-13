8dba0b10890f580ece8f6e66d3fe1109e2f7c559
rohit <rohit.beawar@gmail.com>
2016-09-13 13:48:50 +0530

    Update Text Count method

--- slideQ/Model/SlideDataModel.cs
+++ slideQ/Model/SlideDataModel.cs
@@ -27,16 +27,56 @@ namespace slideQ.Model
             int count = 0;
             foreach (Microsoft.Office.Interop.PowerPoint.Shape shape in slide.Shapes)
             {
+
+                if (shape.Type == MsoShapeType.msoGroup)
+                {
+                    if (shape.GroupItems.Count > 0)
+                    {
+                        count = CheckEveryShapeINGroup(count, shape);
+                    }
+                }
                 if (shape.HasTextFrame == MsoTriState.msoTrue)
                 {
-                    if (shape.TextFrame.HasText == MsoTriState.msoTrue)
-                    {
-                        TextRange Textrange = shape.TextFrame.TextRange;
-                        count += Textrange.Text.Count();
-                    }
+                    count = CheckForHavingText(count, shape);
                 }
             }
             TotalTextCount = count;
+        }
+
+        private static int CheckEveryShapeINGroup(int count, Microsoft.Office.Interop.PowerPoint.Shape shape)
+        {
+            foreach (Microsoft.Office.Interop.PowerPoint.Shape InheritShape in shape.GroupItems)
+            {
+                if (InheritShape.Type == MsoShapeType.msoGroup)
+                {
+                    if (InheritShape.GroupItems.Count > 0)
+                    {
+                        count = CheckEveryShapeINGroup(count, InheritShape);
+                    }
+                }
+                if (InheritShape.HasTextFrame == MsoTriState.msoTrue)
+                {
+                    count = CheckForHavingText(count, InheritShape);
+                }
+            }
+            return count;
+        }
+
+        private static int CheckForHavingText(int count, Microsoft.Office.Interop.PowerPoint.Shape InheritShape)
+        {
+            if (InheritShape.TextFrame.HasText == MsoTriState.msoTrue)
+            {
+                count = GetCharCount(count, InheritShape);
+
+            }
+            return count;
+        }
+
+        private static int GetCharCount(int count, Microsoft.Office.Interop.PowerPoint.Shape shape)
+        {
+            Microsoft.Office.Interop.PowerPoint.TextRange Textrange = shape.TextFrame.TextRange;
+            count += Textrange.Text.Trim().Count();
+            return count;
         }
 
         public int TotalTextCount { get; set; }
