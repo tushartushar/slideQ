aa8b5cf6312d023561ca480f70a8df92483eb735
Rohit Tiwari <rohit.beawar@gmail.com>
2016-09-17 02:53:33 -0700

    BYOB SMell

--- slideQ/Model/SlideDataModel.cs
+++ slideQ/Model/SlideDataModel.cs
@@ -39,11 +39,18 @@ namespace slideQ.Model
                 {
                     count = CheckForHavingText(count, shape);
                 }
+
+                ShapeSize sizeobj = new Model.ShapeSize();
+                sizeobj.Height = shape.Height;
+                sizeobj.Width = shape.Width;
+                sizeobj.Name = shape.Name;
+                ShapeSize.Add(sizeobj);
+
             }
             TotalTextCount = count;
         }
 
-        private static int CheckEveryShapeINGroup(int count, Microsoft.Office.Interop.PowerPoint.Shape shape)
+        private  int CheckEveryShapeINGroup(int count, Microsoft.Office.Interop.PowerPoint.Shape shape)
         {
             foreach (Microsoft.Office.Interop.PowerPoint.Shape InheritShape in shape.GroupItems)
             {
@@ -58,11 +65,16 @@ namespace slideQ.Model
                 {
                     count = CheckForHavingText(count, InheritShape);
                 }
+                ShapeSize sizeobj = new Model.ShapeSize();
+                sizeobj.Height = InheritShape.Height;
+                sizeobj.Width = InheritShape.Width;
+                sizeobj.Name = InheritShape.Name;
+                ShapeSize.Add(sizeobj);
             }
             return count;
         }
 
-        private static int CheckForHavingText(int count, Microsoft.Office.Interop.PowerPoint.Shape InheritShape)
+        private  int CheckForHavingText(int count, Microsoft.Office.Interop.PowerPoint.Shape InheritShape)
         {
             if (InheritShape.TextFrame.HasText == MsoTriState.msoTrue)
             {
@@ -72,15 +84,47 @@ namespace slideQ.Model
             return count;
         }
 
-        private static int GetCharCount(int count, Microsoft.Office.Interop.PowerPoint.Shape shape)
+        private  int GetCharCount(int count, Microsoft.Office.Interop.PowerPoint.Shape shape)
         {
             Microsoft.Office.Interop.PowerPoint.TextRange Textrange = shape.TextFrame.TextRange;
             count += Textrange.Text.Trim().Count();
+
+            for (int index = 0; index < Textrange.Text.Count(); index++)
+            {
+                Microsoft.Office.Interop.PowerPoint.TextRange text = Textrange.Find(Textrange.Text[index].ToString(), index);
+                float sz = text.Font.Size;
+
+                Microsoft.Office.Interop.PowerPoint.ColorFormat clr = text.Font.Color;
+               int flag= TextFontSize.Where(x => x.Size == sz).Count();
+               if (flag == 0)
+                {
+                    CharAttribute attr = new CharAttribute();
+                    attr.Size = sz;
+                    attr.Ch = Textrange.Text[index];
+                    TextFontSize.Add(attr);
+
+                }
+            }
+
             return count;
         }
 
         public int TotalTextCount { get; set; }
 
         public int SlideNo { get; set; }
+
+        public List<CharAttribute> TextFontSize = new List<CharAttribute>();
+        public List<ShapeSize> ShapeSize = new List<ShapeSize>();
+    }
+    public class ShapeSize
+    {
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public string Name { get; set; }
+    }
+    public class CharAttribute
+    {
+        public char Ch { get; set; }
+        public float Size { get; set; }
     }
 }
