12852097fcb06ac0962966bb742b2bff247486d3
Rohit Tiwari <rohit.beawar@gmail.com>
2016-09-25 01:18:36 -0700

    refactor code

--- slideQ/SmellDetectors/BYOBSmellDetector.cs
+++ slideQ/SmellDetectors/BYOBSmellDetector.cs
@@ -19,28 +19,15 @@ namespace slideQ.SmellDetectors
         public List<PresentationSmell> detect()
         {
             List<PresentationSmell> smellList = new List<PresentationSmell>();
-           
-            foreach(SlideDataModel slide in dataModel.SlideDataModelList)
+
+            foreach (SlideDataModel slide in dataModel.SlideDataModelList)
             {
-              List<ShapeSize> SmelldShape=  slide.ShapeSize.Where(x => x.Height < Constants.BYOB_THRESHOLD_HEIGHT || x.Width < Constants.BYOB_THRESHOLD_WIDTH).ToList();
-              List<CharAttribute> SmallTextSmell = slide.TextFontSize.Where(x => x.Size < Constants.BYOB_THRESHOLD_TEXT_SIZE).ToList();
-              if (SmelldShape.Count > 0 || SmallTextSmell.Count >0)
+                List<CharAttribute> SmallTextSmell = slide.TextFontSize.Where(x => x.Size < Constants.BYOB_THRESHOLD_TEXT_SIZE).ToList();
+                if (SmallTextSmell.Count > 0)
                 {
                     PresentationSmell smell = new PresentationSmell();
                     smell.SmellName = Constants.BYOB;
-                    string Cause = "The tool detected the smell since the slide contains";
-                    if (SmelldShape.Count > 0)
-                    {
-                        Cause=Cause+"(" + SmelldShape.Count + ")" +"small shapes(" + string.Join(",", SmelldShape.Select(x=>x.Name).ToArray()) + ")";
-                    }
-                    if (SmelldShape.Count > 0 && SmallTextSmell.Count > 0)
-                    {
-                        Cause = Cause + " and ";
-                    }
-                    if (SmelldShape.Count > 0)
-                    {
-                        Cause = Cause + "(" + SmallTextSmell.Count + ")" + "small character";
-                    }
+                    string Cause = "The tool detected the smell since the slide contains ( " + SmallTextSmell.Count + " ) " + "small character";
                     smell.Cause = Cause;
                     smell.SlideNo = slide.SlideNo;
                     smellList.Add(smell);
