6beecc9a1497192dd8fcdf62037cf40fb3fd8d89
Rohit Tiwari <RohitTiwari92@users.noreply.github.com>
2016-10-08 17:13:41 +0530

    Update Constants.cs

--- slideQ/Threshold/Constants.cs
+++ slideQ/Threshold/Constants.cs
@@ -14,19 +14,19 @@ namespace slideQ
         public static string TEXTHELL = "Text Hell";
         public static string BYOB = "BYOB";
         public static string COLORMANIA = "Color-Mania";
-<<<<<<< HEAD
+
         public static string ItsLasVegas = "Its Las Vegas";
         public static int BYOB_THRESHOLD_HEIGHT = 50;
         public static int BYOB_THRESHOLD_WIDTH = 50;
         public static int BYOB_THRESHOLD_TEXT_SIZE = 16;
-        public static int COLOR_MANIA_THRESHOLD = 2;
+      
         public static int ItsLasVegas_Animation_THRESHOLD = 2;
 
-=======
+
         
         public static int TEXTHELL_THRESHOLD = 200; //characters
         public static int BYOB_THRESHOLD_TEXT_SIZE = 16;
         public static int COLOR_MANIA_THRESHOLD = 4;
->>>>>>> 5ed4e8b588f5ffddd1cc435393015a67c7ab8731
+
     }
 }
